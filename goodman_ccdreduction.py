2a03e08f82ef4c9352bf553fd8b6cf36c4336b1a
David Sanmartim <davidsanm@gmail.com>
2016-07-18 19:55:14 -0400

    Additionally removing keyword INSTRUME from header. It fixed the expected last trouble with the header keywords. I will cross my fingers :P

--- goodman_ccdreduction.py
+++ goodman_ccdreduction.py
@@ -141,17 +141,28 @@ class Main:
             # 3D to 2D
             ccddata = ccddata[0]
             # keywords to remove
-            key_list_to_remove = ['PARAM0', 'PARAM61', 'PARAM62', 'PARAM63', 'NAXIS3']
+            key_list_to_remove = ['PARAM0', 'PARAM61', 'PARAM62', 'PARAM63', 'NAXIS3', 'INSTRUME']
 
             # Keyword to be changed (3 --> 2)
             hdr['N_PARAM'] -= len(key_list_to_remove)
             hdr['NAXIS'] = 2
+
+            # Specific keywords to be removed
             for key in key_list_to_remove:
                 try:
                     if (key in hdr) is True:
                         hdr.remove(keyword=key)
                 except KeyError:
                     pass
+
+            # Removing duplicated keywords
+            key_list = []
+            duplicated_list = []
+            for key in hdr.iterkeys():
+                if key in key_list:
+                    hdr.remove(keyword=key)
+                key_list.append(key)
+
             hdr.add_history('Header and Shape fixed.')
             fits.writeto(os.path.join(output_path, '') + prefix + os.path.basename(_file), ccddata, hdr,
                          clobber=overwrite)
